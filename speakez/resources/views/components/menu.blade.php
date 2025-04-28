8ace1fb1cad53209f154e25863901b4881f019c8
Joseph Martinez <josephmartinezcortes5@gmail.com>
2025-04-28 07:40:47 -0600

    add-implementation

---
+++ speakez/resources/views/components/menu.blade.php
@@ -0,0 +1,10 @@
+<div>
+    <h1>SpeakEz</h1>
+    <nav>
+        <ul>
+            <li><a href="{{ url('/') }}">Inicio</a></li>
+            <li><a href="{{ url('/about') }}">Acerca de</a></li>
+            <li><a href="{{ url('/contact') }}">Contacto</a></li>
+        </ul>
+    </nav>
+</div>
